a51d1153172d0732f1355278e2a0639f12465af9
rubo <ruben@buniatyan.com>
2016-06-30 01:05:06 +0200

    Added docs

--- Utilities/NSDateUtility.cs
+++ Utilities/NSDateUtility.cs
@@ -10,11 +10,25 @@ namespace MissinKit.Utilities
     {
         private static readonly DateTime ReferenceDate = new DateTime(2001, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
+        /// <summary>
+        /// Converts the current <see cref="NSDate"/> object to its equivalent <see cref="DateTime"/> representation.
+        /// </summary>
+        /// <param name="date">An object that represents the value to convert.</param>
+        /// <returns>
+        /// An object that represents the value of the current <see cref="NSDate"/> object converted to <see cref="DateTime"/>.
+        /// </returns>
         public static DateTime ToDateTime(this NSDate date)
         {
             return ReferenceDate.AddSeconds(date.SecondsSinceReferenceDate).ToLocalTime();
         }
 
+        /// <summary>
+        /// Converts the current <see cref="DateTime"/> object to its equivalent <see cref="NSDate"/> representation.
+        /// </summary>
+        /// <param name="dateTime">An object that represents the value to convert.</param>
+        /// <returns>
+        /// An object that represents the date and time of the current <see cref="DateTime"/> object converted to <see cref="NSDate"/>.
+        /// </returns>
         public static NSDate ToNSDate(this DateTime dateTime)
         {
             if (dateTime.Kind == DateTimeKind.Unspecified)
--- Utilities/NSStringUtility.cs
+++ Utilities/NSStringUtility.cs
@@ -16,6 +16,17 @@ namespace MissinKit.Utilities
         private static readonly IntPtr LocalizedStringForKeyValueTableHandle = Selector.GetHandle("localizedStringForKey:value:table:");
         #endregion
 
+        /// <summary>
+        /// Returns an <see cref="NSString"/> object initialized by using a given format string as a template
+        /// into which the remaining argument values are substituted without any localization.
+        /// </summary>
+        /// <param name="format">A format string. This value must not be <code>null</code>.</param>
+        /// <param name="args">A list of arguments to substitute into <code>format</code>.</param>
+        /// <returns>
+        /// An <see cref="NSString"/> object initialized by using <code>format</code> as a template
+        /// into which the values in <code>args</code> are substituted according to the current locale.
+        /// The returned object may be different from the original receiver.
+        /// </returns>
         public static string LocalizedFormat(NSString format, params VariadicArgument[] args)
         {
             if (format == null)
@@ -29,11 +40,54 @@ namespace MissinKit.Utilities
                 return NSString.FromHandle(NSStringInitWithFormatArguments(str.Handle, InitWithFormatArgumentsHandle, format.Handle, varargs.Handle));
         }
 
+        /// <summary>
+        /// Returns a localized version of the string designated by the specified key and residing in the table in Localizable.strings.
+        /// </summary>
+        /// <param name="bundle"></param>
+        /// <param name="key">The key for a string in the table.</param>
+        /// <returns>
+        /// A localized version of the string designated by key in the table.
+        /// This method returns the following when key is <code>null</code> or not found in table:
+        /// <list type="bullet">
+        /// <item>
+        /// <description>If <code>key</code> is <code>null</code>, returns an empty string.</description>
+        /// </item>
+        /// <item>
+        /// <description>If <code>key</code> is not found, returns <code>key</code>.</description>
+        /// </item>
+        /// </list>
+        /// </returns>
         public static NSString LocalizedNSString(this NSBundle bundle, string key)
         {
             return LocalizedNSString(bundle, key, null, null);
         }
 
+        /// <summary>
+        /// Returns a localized version of the string designated by the specified key and residing in the specified table.
+        /// </summary>
+        /// <param name="bundle"></param>
+        /// <param name="key">The key for a string in the table identified by <code>table</code>.</param>
+        /// <param name="value">The value to return if key is <code>null</code> or if a localized string for key can't be found in the table.</param>
+        /// <param name="table">The receiver's string table to search.
+        /// If <code>table</code> is <code>null</code> or is an empty string, the method attempts to use the table in Localizable.strings.</param>
+        /// <returns>
+        /// A localized version of the string designated by key in <code>table</code>.
+        /// This method returns the following when key is <code>null</code> or not found in table:
+        /// <list type="bullet">
+        /// <item>
+        /// <description>If <code>key</code> is <code>null</code> and <code>value</code> is <code>null</code>, returns an empty string.</description>
+        /// </item>
+        /// <item>
+        /// <description>If <code>key</code> is <code>null</code> and <code>value</code> is non-<code>null</code>, returns <code>value</code>.</description>
+        /// </item>
+        /// <item>
+        /// <description>If <code>key</code> is not found and <code>value</code> is <code>null</code> or an empty string, returns <code>key</code>.</description>
+        /// </item>
+        /// <item>
+        /// <description>If <code>key</code> is not found and <code>value</code> is non-<code>null</code> and not empty, return <code>value</code>.</description>
+        /// </item>
+        /// </list>
+        /// </returns>
         public static NSString LocalizedNSString(this NSBundle bundle, string key, string value, string table)
         {
             if (bundle == null)
@@ -55,11 +109,6 @@ namespace MissinKit.Utilities
                 NSString.ReleaseNative(tableHandle);
 
             return Runtime.GetNSObject<NSString>(localizedHandle);
-        }
-
-        public static NSString ToNSString(this string str)
-        {
-            return (NSString) str;
         }
 
         #region Objective-C Bindings

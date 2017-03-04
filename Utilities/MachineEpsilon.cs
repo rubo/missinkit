e0a978a8f6bebd4fd73928673d24699886b4fde8
rubo <ruben@buniatyan.com>
2017-03-04 15:06:46 +0100

    Added machine epsilon constants for ARM systems

---
+++ Utilities/MachineEpsilon.cs
@@ -0,0 +1,20 @@
+﻿// Copyright 2017 Ruben Buniatyan
+// Licensed under the MIT License. For full terms, see LICENSE in the project root.
+
+namespace MissinKit.Utilities
+{
+    public static class MachineEpsilon
+    {
+        /// <summary>
+        /// Represents the smallest positive <see cref="double"/> value on ARM systems that is greater than zero. This field is constant.
+        /// </summary>
+        /// <seealso cref="double.Epsilon"/>
+        public const double Double = 2.2250738585072014E-308;
+
+        /// <summary>
+        /// Represents the smallest positive <see cref="float"/> value on ARM systems that is greater than zero. This field is constant.
+        /// </summary>
+        /// <seealso cref="float.Epsilon"/>
+        public const float Single = 1.175494351E-38F;
+    }
+}

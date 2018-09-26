dcecd8894e66159af2a85d5d40a144269a998491
rubo <ruben@buniatyan.com>
2018-09-26 22:27:44 +0200

    Refactored and revised

--- Utilities/L10n.cs
+++ Utilities/L10n.cs
@@ -4,6 +4,7 @@
 #pragma warning disable 1591
 
 using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using Foundation;
 using ObjCRuntime;
@@ -23,21 +24,32 @@ namespace MissinKit.Utilities
         /// <summary>
         /// Overrides <see cref="NSLocale.CurrentLocale"/> with a locale for the identifier specified.
         /// </summary>
-        /// <param name="localeId">Locale identifier</param>
-        public static void OverrideCurrentLocale(string localeId)
+        /// <param name="localeIdentifier">Locale identifier</param>
+        /// <remarks>
+        /// If the current locale is already overridden, it must be restored by
+        /// invoking the <see cref="RestoreCurrentLocale"/> method before overriding again.
+        /// </remarks>
+        public static void OverrideCurrentLocale(string localeIdentifier)
         {
-            if (string.CompareOrdinal(localeId, _locale?.Identifier) == 0)
+            if (localeIdentifier == null)
+                throw new ArgumentNullException(nameof(localeIdentifier));
+
+            if (localeIdentifier.Equals(_locale?.Identifier, StringComparison.OrdinalIgnoreCase))
                 return;
 
-            _locale = NSLocale.FromLocaleIdentifier(localeId);
+            if (IsOverridden)
+            {
+                Debug.WriteLine($"{nameof(L10n)}: The current locale is already overridden. To override again, restore it.");
+
+                return;
+            }
+
+            _locale = NSLocale.FromLocaleIdentifier(localeIdentifier);
 
             var path = NSBundle.MainBundle.PathForResource(_locale.LanguageCode, "lproj")
                 ?? NSBundle.MainBundle.PathForResource("Base", "lproj");
 
             BundleForCurrentLocale = string.IsNullOrEmpty(path) ? NSBundle.MainBundle : NSBundle.FromPath(path);
-
-            if (IsOverridden)
-                return;
 
             var method = class_getClassMethod(NSLocale.CurrentLocale.ClassHandle, CurrentLocaleSelectorHandle);
 
